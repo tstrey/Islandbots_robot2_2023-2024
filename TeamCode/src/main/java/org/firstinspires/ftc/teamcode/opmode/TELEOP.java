50674fe6255176220e438193c1e4193f90941f79
Sanyok6 <>
2023-11-23 19:37:51 -0500

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TELEOP.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Twist2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.TankDrive;
+
+@TeleOp
+public class TELEOP extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+        // li_servo stands for "left intake servo"
+        // We use the CRServo class because the intake servos are constant rotation servos
+        CRServo li_servo = hardwareMap.crservo.get("li_servo");
+        CRServo ri_servo = hardwareMap.crservo.get("ri_servo");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            drive.setDrivePowers(new PoseVelocity2d(
+                    new Vector2d(
+                            -gamepad1.left_stick_y / 2,
+                            -gamepad1.left_stick_x / 2
+                    ),
+                    -gamepad1.right_stick_x / 3
+            ));
+
+            drive.updatePoseEstimate();
+
+            // this controls the intake
+            if (gamepad1.left_trigger > 0 || gamepad1.right_trigger > 0) {
+                li_servo.setPower(-1);
+                ri_servo.setPower(1);
+            } else if (gamepad1.left_bumper || gamepad1.right_bumper) {
+                li_servo.setPower(1);
+                ri_servo.setPower(-1);
+            } else {
+                li_servo.setPower(0);
+                ri_servo.setPower(0);
+            }
+
+            telemetry.addData("x", drive.pose.position.x);
+            telemetry.addData("y", drive.pose.position.y);
+            telemetry.addData("heading", drive.pose.heading);
+            telemetry.update();
+        }
+    }
+}
